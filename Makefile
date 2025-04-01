4d0f87f920b8116f44e253fda97bb2db87e4d29e
Rong "Mantle" Bao <webmaster@csmantle.top>
2025-04-01 12:14:33 +0800

    impl: add microarch-independent optims
    Cycle count: 171941 (baseline) -> 142008

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ CROSS_CC := $(CROSS_COMPILE)gcc
 CROSS_LD := $(CROSS_COMPILE)ld
 CROSS_OBJCOPY := $(CROSS_COMPILE)objcopy
 CFLAGS := -O3 -Ilib -Isrc -fno-builtin -static
-CROSS_CFLAGS := -march=rv64gc -mcmodel=medany
+CROSS_CFLAGS := -march=rv64gc_zbb_zbkb -mcmodel=medany
 BAREMETAL_CFLAGS := -nostdlib
 BENCH_LEN := 256
 
---
+++ src/chacha20.c
@@ -0,0 +1,57 @@
+#include <chacha20.h>
+
+static inline u32 ROL32(u32 v, u8 n) {
+  u32 result;
+  __asm__ volatile("rolw\t%0,%1,%2" : "=r"(result) : "r"(v), "r"(n));
+  return result;
+}
+
+static inline u32 ROR32(u32 v, u8 n) {
+  u32 result;
+  __asm__ volatile("rorw\t%0,%1,%2" : "=r"(result) : "r"(v), "r"(n));
+  return result;
+}
+
+static inline u32 RORC32(u32 v, const u8 n) {
+  u32 result;
+  __asm__ volatile("roriw\t%0,%1,%2" : "=r"(result) : "r"(v), "I"(n));
+  return result;
+}
+
+/* QUARTERROUND updates a, b, c, d with a ChaCha "quarter" round. */
+#define QUARTERROUND(a, b, c, d)                                                                   \
+  do {                                                                                             \
+    x[a] += x[b];                                                                                  \
+    x[d] = RORC32((x[d] ^ x[a]), 16);                                                              \
+    x[c] += x[d];                                                                                  \
+    x[b] = RORC32((x[b] ^ x[c]), 20);                                                              \
+    x[a] += x[b];                                                                                  \
+    x[d] = RORC32((x[d] ^ x[a]), 24);                                                              \
+    x[c] += x[d];                                                                                  \
+    x[b] = RORC32((x[b] ^ x[c]), 25);                                                              \
+  } while (0)
+
+/* chacha_core performs 20 rounds of ChaCha on the input words in
+ * |input| and writes the 64 output bytes to |output|. */
+void chacha20(chacha_buf *output, const u32 input[16]) {
+  u32 x[16];
+
+  for (int i = 0; i < 16; i++) {
+    x[i] = input[i];
+  }
+
+  for (int i = 20; i > 0; i -= 2) {
+    QUARTERROUND(0, 4, 8, 12);
+    QUARTERROUND(1, 5, 9, 13);
+    QUARTERROUND(2, 6, 10, 14);
+    QUARTERROUND(3, 7, 11, 15);
+    QUARTERROUND(0, 5, 10, 15);
+    QUARTERROUND(1, 6, 11, 12);
+    QUARTERROUND(2, 7, 8, 13);
+    QUARTERROUND(3, 4, 9, 14);
+  }
+
+  for (int i = 0; i < 16; ++i) {
+    output->u[i] = x[i] + input[i];
+  }
+}
